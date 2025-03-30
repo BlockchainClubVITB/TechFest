58cc2cb3babbe4a7da7692471c54ebccc76f7c6a
Dhairya Gothi <dhairyag31@gmail.com>
2025-03-30 18:44:01 +0530

    dg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
 });
